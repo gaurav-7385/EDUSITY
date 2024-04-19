b8fae36b9d5f78eb78a66c621d299c089256c194
gaurav-7385 <153974792+gaurav-7385@users.noreply.github.com>
2024-04-19 11:38:40 +0530

    Add files via upload

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message
+          <img src={msg_icon} />
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>
+            <img src={mail_icon} />
+            Contact@gauravshinde.dev
+          </li>
+          <li>
+            <img src={phone_icon} />
+            +1 123-456-7890
+          </li>
+          <li>
+            <img src={location_icon} />
+            77 Massachusetts Ave, Cambridge MA 02139, United States
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form>
+          <label>Your Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your Name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter Your Mobile Number"
+            required
+          />
+          <label>Write Your Message Here!!</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit Now <img src={white_arrow} />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
